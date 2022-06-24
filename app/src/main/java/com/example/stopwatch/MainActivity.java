6f1eeed5b02af118867fd1125d95191d208c7cea
ANIMESH PANDEY <animeshpandey99@gmail.com>
2022-06-24 22:04:17 +0530

    initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    int second = 0; //intial point of stopwatch
+    boolean running;
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        runTimer();
+    }
+
+    public void onStart(View view) {
+        running = true;
+    }
+
+    public void onPause(View view) {
+        running = false;
+    }
+
+    public void onReset(View view) {
+        running = false;
+        second = 0;
+    }
+
+    public void runTimer(){
+        final TextView timer = findViewById(R.id.timer);
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = second/ 3600;
+                int minutes = (second%3600)/60;
+                int seconds = second%60;
+
+                if (running){
+                    second++;
+                }
+
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, seconds);
+                timer.setText(time);
+                handler.postDelayed(this,1000);
+
+            }
+        });
+
+
+    }
+}
